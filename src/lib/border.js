eeb3a2b395021557344719480174047326c01dc3
Luiz Gustavo <luizgustavolgss@gmail.com>
2020-09-14 13:21:44 -0300

    [API/Documentation] API: Replacing switch by Cond; Documentation: Improving examples

--- src/lib/border.js
+++ src/lib/border.js
@@ -1,6 +1,6 @@
 import { node, tran } from '../node.js';
 import { Supp, preserveR, keyed } from '../rect.js';
-import { px, len } from '../coord.js';
+import { px, len, addCoord } from '../coord.js';
 import { Tree } from '../tree.js';
 
 export const border = (b, tree) => {
@@ -48,3 +48,108 @@ export const border = (b, tree) => {
 };
 
 export const _border = b => tree => border(b, tree);
+
+export const externalBorder = (b, tree) => {
+    const rect = tree.elem;
+    const innerPos = node();
+    const innerSiz = node();
+    const color = tran(b, ({ color }) => color);
+    const width = tran(b, ({ width }) => width);
+
+    const outterPos = tran(width, rect.layout.pos, (w, p) =>
+        addCoord(p, [px(-w), px(-w)])
+    );
+    const outterSiz = tran(width, rect.layout.siz, (w, s) =>
+        addCoord(s, [px(2 * w), px(2 * w)])
+    );
+
+    const s = Supp({
+        layout: {
+            pos: outterPos,
+            siz: outterSiz
+        },
+        data: keyed(border, {
+            node: b,
+            outter: true,
+            inner: false
+        }),
+        style: {
+            color: color
+        }
+    });
+    return Tree(
+        s,
+        Tree(
+            preserveR(rect, {
+                layout: {
+                    pos: tran(width, w => [px(w), px(w)]),
+                    siz: tran(width, w => [
+                        len(100, -2 * w),
+                        len(100, -2 * w)
+                    ])
+                },
+                data: keyed(border, {
+                    node: b,
+                    inner: true,
+                    outter: false
+                })
+            }),
+            tree.children
+        )
+    );
+};
+
+export const _externalBorder = b => tree => externalBorder(b, tree);
+
+export const seamlessBorder = (b, tree) => {
+    const rect = tree.elem;
+    const color = tran(b, ({ color }) => color);
+    const width = tran(b, ({ width }) => width);
+
+    const outterPos = tran(width, rect.layout.pos, (w, p) =>
+        addCoord(p, [px(-w / 2), px(-w / 2)])
+    );
+    const outterSiz = tran(width, rect.layout.siz, (w, s) =>
+        addCoord(s, [px(w), px(w)])
+    );
+
+    const innerPos = tran(width, w => [px(w), px(w)]);
+    const innerSiz = tran(width, w => [
+        len(100, -2 * w),
+        len(100, -2 * w)
+    ]);
+
+    const s = Supp({
+        layout: {
+            pos: outterPos,
+            siz: outterSiz
+        },
+        data: keyed(border, {
+            node: b,
+            outter: true,
+            inner: false
+        }),
+        style: {
+            color: color
+        }
+    });
+    return Tree(
+        s,
+        Tree(
+            preserveR(rect, {
+                layout: {
+                    pos: innerPos,
+                    siz: innerSiz
+                },
+                data: keyed(border, {
+                    node: b,
+                    inner: true,
+                    outter: false
+                })
+            }),
+            tree.children
+        )
+    );
+};
+
+export const _seamlessBorder = b => tree => seamlessBorder(b, tree);
--- src/lib/cond.js
+++ src/lib/cond.js
@@ -4,47 +4,47 @@ import { Supp } from '../rect.js';
 import { Tree } from '../tree.js';
 import { container } from './container.js';
 
-export const switcher = (route, routeRectMap) => {
-    const children = node();
-    const routeMap = mapValuesObj(routeRectMap, val => {
-        const destroy = val.destroy ? val.destroy : false;
-        const show = node(false);
-        const rectT = container(show, val.content || val);
-        return {
-            show,
-            rectT,
-            destroy
-        };
-    });
-    const siz = node([100, 100]);
-    tran([route], () => {
-        const newRoute = route.val;
-        children.val = iterate(routeMap, ([rou, val]) => {
-            const { show, rectT, destroy } = val;
-            if (rou == newRoute) {
-                show.val = true;
-                return rectT;
-            } else if (destroy) {
-                return null;
-            } else {
-                show.val = false;
-                return rectT;
-            }
-        }).filter(isNotNull);
-        // hack to fiz some problems when switch happens
-        siz.val = [{ rel: 100, px: 0.01 }, 100];
-        siz.val = [100, 100];
-    });
-    return Tree(
-        Supp({
-            layout: {
-                pos: [0, 0],
-                siz
-            }
-        }),
-        children
-    );
-};
+// export const switcher = (route, routeRectMap) => {
+//     const children = node();
+//     const routeMap = mapValuesObj(routeRectMap, val => {
+//         const destroy = val.destroy ? val.destroy : false;
+//         const show = node(false);
+//         const rectT = container(show, val.content || val);
+//         return {
+//             show,
+//             rectT,
+//             destroy
+//         };
+//     });
+//     const siz = node([100, 100]);
+//     tran([route], () => {
+//         const newRoute = route.val;
+//         children.val = iterate(routeMap, ([rou, val]) => {
+//             const { show, rectT, destroy } = val;
+//             if (rou == newRoute) {
+//                 show.val = true;
+//                 return rectT;
+//             } else if (destroy) {
+//                 return null;
+//             } else {
+//                 show.val = false;
+//                 return rectT;
+//             }
+//         }).filter(isNotNull);
+//         // hack to fiz some problems when switch happens
+//         siz.val = [{ rel: 100, px: 0.01 }, 100];
+//         siz.val = [100, 100];
+//     });
+//     return Tree(
+//         Supp({
+//             layout: {
+//                 pos: [0, 0],
+//                 siz
+//             }
+//         }),
+//         children
+//     );
+// };
 
 export const Cond = (route, routeRectMap) => {
     const children = node([]);
